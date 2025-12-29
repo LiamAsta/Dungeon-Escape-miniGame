de47dac43d8e73630e08e7f1609e97e94a65b74a
LiamAsta <william-astarita24@outlook.com>
2025-12-29 20:48:10 +0100

    Update game.c

--- game.c
+++ game.c
@@ -3,6 +3,15 @@
 #include <string.h>
 
 
+typedef enum {
+    RUNNING,
+    WIN,
+    EXIT
+}GameState ;
+
+GameState game1;
+
+
 #define ROWS 6
 #define COLS 10
 
@@ -32,13 +41,10 @@ void setEscapeCoord(char map[ROWS][COLS + 1]) {
         }
     }
 }
-
-
 void printMap (char map[ROWS][COLS + 1]) {
     for (int i = 0; i < ROWS; i++) {
 
         for (int j = 0; j < COLS ; j++) {
-            
             printf("%c", map[i][j]);
 
         }
@@ -49,16 +55,9 @@ void printMap (char map[ROWS][COLS + 1]) {
 
     printf("\n");
 }
-
 void clearScreen() {
     printf("\033[2J\033[H");
 }
-
-
-
-
-
-
 
 
 
@@ -96,18 +95,16 @@ void spawnPlayer(Player *p, char map[ROWS][COLS + 1]) {
 /* update the map with the new player position */
 void updateMap (char map[ROWS][COLS + 1], Player *p) {
 
-    char mappa_iniziale[ROWS][COLS + 1] = {
-        "##########",
-        "#..S....##",
-        "#.#####.##",
-        "#.....##.#",
-        "####...E##",
-        "##########"
-    };
+    for (int i = 0 ; i < ROWS ; i++) {
+        for (int j = 0 ; j < COLS ; j++) {
 
-    mappa_iniziale[p->posY][p->posX] = '@';
-    for (int i = 0; i < ROWS; i++) {
-        strncpy(map[i], mappa_iniziale[i], COLS + 1);
+            if (map[i][j] == '@') map[i][j] = '.';
+
+            if (i == p->posY && j == p->posX) {
+                map[i][j] = '@';
+            }
+
+        }
     }
 
     clearScreen();
@@ -115,34 +112,14 @@ void updateMap (char map[ROWS][COLS + 1], Player *p) {
 }
 
 
-/* function that updates the game only if the player doesn go into a wall */
-void update (char map[ROWS][COLS + 1], Player *p, int x, int y) {
-    if (map[y][x] != '#' && map[y][x] != '\n') {
-        p->posX = x;
-        p->posY = y;
-        p->numMosse--;
-        updateMap(map, p);
-    }
-}
-
 
 /* Check if the input move goes into a wall or not, if yes it will return 0, else the move is possible, so return 1 */
 int isThisMovePossible (int x, int y, char map [ROWS][COLS + 1]) {
-    if (map[y][x] != '#' && map[y][x] != '\n') return 1;
+    if (map[y][x] != '#') return 1;
     return 0;
 }
 
 
-
-
-
-typedef enum {
-    RUNNING,
-    WIN,
-    EXIT
-}GameState ;
-
-GameState game1;
 
 
 
@@ -163,10 +140,10 @@ int main (void) {
 
      while (game1 == RUNNING) {
 
-         clearScreen();printMap(map);
-
          printf("MOSSE RIMANENTI : %d\n", player.numMosse - 1);
+
          char key = getchar();
+         do { key = getchar(); } while (key == '\n');
 
          int x = player.posX;
          int y = player.posY;
@@ -181,7 +158,10 @@ int main (void) {
                }
 
         if (isThisMovePossible(x,y,map)) {
-            update(map, &player, x, y);
+            player.posX = x;
+            player.posY = y;
+            player.numMosse--;
+            updateMap(map, &player);
         } else {player.numMosse--; clearScreen();printMap(map); }
 
         if (player.posX == escapeX && player.posY == escapeY) {
@@ -201,4 +181,3 @@ int main (void) {
 
 
 }
-
